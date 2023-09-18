9700313d5aaba89bd5cf1714d31471aa3a4f74e6
rrachea <rrachelsng@gmail.com>
2023-09-18 13:29:11 +0800

    added more params to check for get customer
    got error w dateJoined and totalCap so commented it out first

--- src/test/java/com/trading/application/customer/controller/TestCustomerController.java
+++ src/test/java/com/trading/application/customer/controller/TestCustomerController.java
@@ -1,20 +1,13 @@
 package com.trading.application.customer.controller;
 
-import com.trading.application.customer.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,8 +24,12 @@ public class TestCustomerController {
         String userId = "5cJ0NI3WpPLi9hCQKZG0";
 
         this.mockMvc.perform(get("/customer/" + userId)).andDo(print()).andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("dooshik"));
-
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(userId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("dooshik"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("kim"))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.dateJoined").value("14/9/2023"))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.totalCapitalAvailable").value(999999))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("moonsan@gmail.com"));
 
     }
 }
