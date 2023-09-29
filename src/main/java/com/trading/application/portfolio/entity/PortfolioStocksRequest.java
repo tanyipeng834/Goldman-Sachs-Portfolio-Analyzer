e1713de0777c8870d2ccb46dde5d8f2327a31c34
rrachea <rrachelsng@gmail.com>
2023-09-29 09:50:47 +0800

    create portfolioreq and portfoliostockreq entity

---
+++ src/main/java/com/trading/application/portfolio/entity/PortfolioStocksRequest.java
@@ -0,0 +1,34 @@
+package com.trading.application.portfolio.entity;
+
+import com.trading.application.portfoliostock.entity.PortfolioStock;
+
+import java.util.List;
+
+public class PortfolioStocksRequest {
+    private List<PortfolioStock> added;
+    private List<PortfolioStock> deleted;
+    private List<PortfolioStock> updated;
+    public List<PortfolioStock> getAdded() {
+        return added;
+    }
+
+    public void setAdded(List<PortfolioStock> added) {
+        this.added = added;
+    }
+
+    public List<PortfolioStock> getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(List<PortfolioStock> deleted) {
+        this.deleted = deleted;
+    }
+
+    public List<PortfolioStock> getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(List<PortfolioStock> updated) {
+        this.updated = updated;
+    }
+}
