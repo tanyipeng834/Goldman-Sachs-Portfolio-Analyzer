3f330e06027f1a2875294335a271e30836d04798
xuanli286 <xuanli743@gmail.com>
2023-10-01 23:51:12 +0800

    add cors for StockController

--- src/main/java/com/trading/application/stock/controller/StockController.java
+++ src/main/java/com/trading/application/stock/controller/StockController.java
@@ -6,16 +6,14 @@ import com.trading.application.stock.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
 @RestController
 @EnableCaching
 @RequestMapping("/stock")
+@CrossOrigin(origins = "http://localhost:8080")
 public class StockController {
     @Autowired
     private StockService stockService;
