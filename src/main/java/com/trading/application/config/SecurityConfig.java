1d89cde28440aca91a39a2ef3198f1631db9e489
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-12 20:44:41 +0800

    implemented authorization middleware on the backend

---
+++ src/main/java/com/trading/application/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.trading.application.config;
+
+import com.trading.application.middleware.AudienceValidator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Configures our application with Spring Security to restrict access to our API endpoints.
+ */
+
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.oauth2Login();
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        /*
+        By default, Spring Security does not validate the "aud" claim of the token, to ensure that this token is
+        indeed intended for our app. Adding our own validator is easy to do:
+        */
+
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+                JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+}
--- src/main/java/com/trading/application/customer/controller/CustomerController.java
+++ src/main/java/com/trading/application/customer/controller/CustomerController.java
@@ -1,13 +1,20 @@
 package com.trading.application.customer.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.trading.application.CustomError;
 import com.trading.application.customer.entity.Customer;
 import com.trading.application.customer.service.CustomerService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -21,22 +28,42 @@ public class CustomerController {
     // create new customer
     @PostMapping
     @RequestMapping("/")
-    public String createCustomer(@RequestBody Customer customer) throws ExecutionException, InterruptedException {
-        return customerService.createCustomer(customer);
+    public ResponseEntity<Object> createCustomer(@RequestBody Customer customer) throws ExecutionException, InterruptedException,UnirestException {
+
+        String accessToken = generateJWT();
+        Map<String, Object> customerResponseBody = new HashMap<>();
+        customerResponseBody.put("customerData", customerService.createCustomer(customer));
+        customerResponseBody.put("token", accessToken);
+
+        return new ResponseEntity<>(customerResponseBody, HttpStatus.OK);
+
+
+
     }
 
     // get customer by id
     @GetMapping
     @RequestMapping("/{id}")
     public ResponseEntity<Object> getCustomerById(@PathVariable String id) throws ExecutionException, InterruptedException {
-//        return customerService.getCustomer(id);
+
 
         try {
             if (customerService.getCustomer(id) == null) {
                 return new ResponseEntity<>(new CustomError(404, "Customer not found"), HttpStatus.NOT_FOUND);
             }
 
-            return new ResponseEntity<>(customerService.getCustomer(id), HttpStatus.OK);
+            String accessToken = generateJWT();
+
+
+// Extract the access_token from the JSON response
+
+
+            // Create a response object containing the customer data and the token
+            Map<String, Object> customerResponseBody = new HashMap<>();
+            customerResponseBody.put("customerData", customerService.getCustomer(id));
+            customerResponseBody.put("token", accessToken);
+
+            return new ResponseEntity<>(customerResponseBody, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(new CustomError(500, "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -71,6 +98,19 @@ public class CustomerController {
     @RequestMapping("/getcapital/{userId}")
     public float getCapital(@PathVariable String userId) throws ExecutionException, InterruptedException {
         return customerService.getCustomerCapital(userId);
+    }
+
+
+    public String generateJWT() throws UnirestException {
+        HttpResponse<String> response = Unirest.post("https://dev-4pxn4zbtcuoww57l.us.auth0.com/oauth/token")
+                .header("content-type", "application/json")
+                .body("{\"client_id\":\"5BLESP05RJ9IJ39tX5GCKYMjCpaGfcBZ\",\"client_secret\":\"0rGysyH68zlJjtbKm38IoZ6ZDhL8bJ-Ydpps8Uo4dKCHFieXavk5tnST_jJek1mj\",\"audience\":\"https://goldman.com\",\"grant_type\":\"client_credentials\"}")
+                .asString();
+        String responseBody = response.getBody();
+        JSONObject json = new JSONObject(responseBody);
+        String accessToken = json.getString("access_token");
+        return accessToken;
+
     }
 
 }
