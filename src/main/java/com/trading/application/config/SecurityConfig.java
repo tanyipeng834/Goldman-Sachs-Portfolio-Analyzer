bcc9378408e9e6191fef78e5e4f23c70320435fb
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-13 02:41:02 +0800

    working on middleware

--- src/main/java/com/trading/application/config/SecurityConfig.java
+++ src/main/java/com/trading/application/config/SecurityConfig.java
@@ -4,13 +4,20 @@ import com.trading.application.middleware.AudienceValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * Configures our application with Spring Security to restrict access to our API endpoints.
@@ -27,13 +34,45 @@ public class SecurityConfig {
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
 
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedOrigin("http://localhost:8080"); // Allow requests from this origin
+       configuration.addAllowedHeader("*");
+        configuration.setExposedHeaders(Arrays.asList("Authorization")); // Expose 'Authorization' header
+
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+
+       // Expose 'Authorization' header
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+//.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//        csrf(AbstractHttpConfigurer::disable)?
+
+//        http.authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/stockprice/**").authenticated()
+//                        .requestMatchers(new AntPathRequestMatcher("/customer/**")).permitAll()
+//                );
+
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/customer/**").permitAll().anyRequest().authenticated()
+
+                .requestMatchers("/customer/**").permitAll()
+                .requestMatchers("/stockprice/**").authenticated()
+
 
         );
+
+
         return http.build();
     }
 
