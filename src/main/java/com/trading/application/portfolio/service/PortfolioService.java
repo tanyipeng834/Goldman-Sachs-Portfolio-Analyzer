acb89a86ef2844582956a556f9e2639cb33c5244
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-17 00:44:19 +0800

    still working on code

--- src/main/java/com/trading/application/portfolio/service/PortfolioService.java
+++ src/main/java/com/trading/application/portfolio/service/PortfolioService.java
@@ -1,5 +1,6 @@
 package com.trading.application.portfolio.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.cloud.firestore.FirestoreException;
 import com.trading.application.logs.entity.AccessLog;
 import com.trading.application.logs.service.AccessLogService;
@@ -8,13 +9,20 @@ import com.trading.application.portfolio.entity.PortfolioStocksRequest;
 import com.trading.application.portfolio.repository.PortfolioRepository;
 import com.trading.application.portfoliostock.entity.PortfolioStock;
 import com.trading.application.portfoliostock.service.PortfolioStockService;
+import com.trading.application.stockprice.entity.StockPrice;
+import com.trading.application.stockprice.service.StockPriceService;
+import com.trading.application.stockprice.service.StockPricesService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.sound.sampled.Port;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
@@ -27,8 +35,9 @@ public class PortfolioService {
 
     @Autowired
     private AccessLogService accessLogService = new AccessLogService();
-
-    public ResponseEntity<String> createPortfolio(Portfolio portfolio, HttpServletRequest request) {
+    @Autowired
+    private StockPricesService stockPricesService = new StockPricesService()
+        public ResponseEntity<String> createPortfolio(Portfolio portfolio, HttpServletRequest request) {
         try {
             String result = portfolioRepo.createPortfolio(portfolio);
             // add to access log after portfolio successfully created in firebase
@@ -147,6 +156,119 @@ public class PortfolioService {
     public ArrayList<Portfolio> getAllPublicPortfolios() throws ExecutionException, InterruptedException {
         return portfolioRepo.getAllPublicPortfolios();
     }
+
+
+
+    public Map<String, List<PortfolioStock>> rebalance(Portfolio portfolio)
+
+    {
+
+        String startDate = portfolio.getDateCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
+
+        YearMonth createyearMonth = YearMonth.parse(startDate, formatter);
+
+
+
+        Map<String,List<PortfolioStock>> portMap= portfolio.getPortStock();
+
+
+
+
+
+            // You can access the value associated with the key using map.get(key)
+
+            rebalanceStock(createyearMonth,portMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public List<PortfolioStock> rebalanceStock(YearMonth stockTime,Map<String,List<PortfolioStock>> portMap) throws ExecutionException, InterruptedException, JsonProcessingException {
+        YearMonth createdTime = stockTime;
+         // Initialize the list
+        YearMonth currentYearMonth = YearMonth.now();
+
+        int counter = -1;
+        while (createdTime.isBefore(currentYearMonth) || createdTime.equals(currentYearMonth)) {
+            // Add logic here to process PortfolioStock and add to newPortfolioStock
+            // For example, if PortfolioStock is retrieved from some source:
+            // have to create a new list with the 1st object to take care of update
+            float portValue = 0.0f;
+
+            for (Map.Entry<String, List<PortfolioStock>> entry : portMap.entrySet()) {
+                String key = entry.getKey();
+
+                if(counter>=0){
+                    // Get the previous quantity of the stock from the last rebalancing
+                    PortfolioStock stock =entry.getValue().get(counter);
+                    float idealAmount = stock.get
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+
+                else{
+                    // No need to add the stock in to the object
+                    PortfolioStock stock =entry.getValue().get(0);
+                    YearMonth futureTime = stockTime.plusMonths(3);
+                    Object stockPrice =stockPricesService.getMonthlyPriceFromDate(key,String.valueOf(futureTime.getMonthValue()),String.valueOf(futureTime.getYear()));
+                    if(stockPrice instanceof StockPrice){
+                        portValue+= ((StockPrice) stockPrice).getClosePrice()*stock.getQuantity();
+
+                    }
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+                // Process the value here
+            }
+
+
+            createdTime = createdTime.plusMonths(3);
+            counter+=1;
+        }
+
+
+
+
+
+
+    }
+
+
+
 
 
 }
