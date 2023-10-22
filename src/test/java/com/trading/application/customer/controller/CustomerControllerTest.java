628240eebaa41a7a295ee0dd523b1a7ade200c94
rrachea <rrachelsng@gmail.com>
2023-10-22 16:03:48 +0800

    customer controller testing completed

--- src/test/java/com/trading/application/customer/controller/CustomerControllerTest.java
+++ src/test/java/com/trading/application/customer/controller/CustomerControllerTest.java
@@ -47,7 +47,6 @@ class CustomerControllerTest {
 //    String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpiQ09XZjV6WkZYanZ1YUxBcDNGSiJ9" +
 //        ".eyJpc3MiOiJodHRwczovL2Rldi00cHhuNHpidGN1b3d3NTdsLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiI1QkxFU1AwNVJKOUlKMzl0WDVHQ0tZTWpDcGFHZmNCWkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9nb2xkbWFuLmNvbSIsImlhdCI6MTY5NzI2MzI3MSwiZXhwIjoxNjk3MzQ5NjcxLCJhenAiOiI1QkxFU1AwNVJKOUlKMzl0WDVHQ0tZTWpDcGFHZmNCWiIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.YaNqWvEjip13dDKBTNpqLO7emDnKk0GiXgQdvUDHAkt7A5d658WxehkzsgHPOjtNqGYOBJJ3zVKyToz5uzg05qivSFdyXO61-KXGqoojJ9xQON7k2EMUcIF-mHa3cbTbARIZhUYerH3wFTclVXGUFX-qpLmF1Lm43sgkWB0OWkDnDOg1gpTPvcU9XFn7VKyws-2hGthmFPIeVXbbIVFKiTmOefV0HRDMQBd6AdvEUEXAGSeoOhVJ0QfnkoaZtKm5fJRGsnlMA5lplfn_idAUHHe3WLtmuWryYrXkEtzCMQmrQb_aTIvq7YdtXUjvP_KZScfP6oh4AQYSQRyR2bk_9A";
 
-
     /**
      * Should create customer.
      *
@@ -56,144 +55,98 @@ class CustomerControllerTest {
     @Test
     public void shouldCreateCustomer() throws Exception {
 
-        // Create a Customer instance
         Customer customer = new Customer();
 
-        // Create the expected response
-        String accessToken = "abc";
         Map<String, Object> customerResponseBody = new HashMap<>();
         customerResponseBody.put("customerData", customer);
-        customerResponseBody.put("token", accessToken);
+        customerResponseBody.put("token", token);
         ResponseEntity<Object> expectedResponse = new ResponseEntity<>(customerResponseBody, HttpStatus.OK);
 
         when(customerService.createCustomer(customer)).thenReturn(String.valueOf(customer));
 
-        // Call the controller method
         ResponseEntity<Object> actualResponse = customerController.createCustomer(customer);
 
-        // Assert the HTTP status
+        assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
+
+
+    }
+
+    /**
+     * Should get customer by id.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void shouldGetCustomerById() throws Exception {
+        String id = "testid";
+
+        Customer expectedCustomer = new Customer();
+        when(customerService.getCustomer(id)).thenReturn(expectedCustomer);
+
+        ResponseEntity<Object> actualResponse = customerController.getCustomerById(id);
+
         assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
 
+    }
+    /**
+     * Should update customer name.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void shouldUpdateCustomerName() throws Exception {
 
-    }}
+        String name = "new_name";
+        String id = "auth0_65192b7fe51e9baa2513bb99";
 
-//    /**
-//     * Should get customer by id.
-//     *
-//     * @throws Exception the exception
-//     */
-//    @Test
-//    void shouldGetCustomerById() throws Exception {
-//        String id = "testid";
-//        mockMvc.perform(get("/customer/" + id).header("authorization", "Bearer " + token)).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.customerData.id").value(id))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.customerData.email").value("test@gmail.com"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.customerData.name").value("test.user.2021"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.customerData.totalCapitalAvailable").value(10000))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.customerData.updatedAt").value("2023-10-01T08:19:57.640Z"));}
-//
-//    /**
-//     * Should update customer name.
-//     *
-//     * @throws Exception the exception
-//     */
-//    @Test
-//    void shouldUpdateCustomerName() throws Exception {
-//
-//        String id = "testid";
-//        String name = "new_name";
-//        String email = "test@gmail.com";
-//        String picture = "https://s.gravatar.com/avatar/f46f055e3618f5939cfc4a8e6572279c?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fxu.png";
-//        String updatedAt = "2023-10-01T08:19:57.640Z";
-//        //to confirm data type
-//        float totalCapitalAvailable = 10000;
-//
-//        String body = String.format(
-//                "{\"email\":\"%s\",\"id\":\"%s\",\"name\":\"%s\",\"picture\":\"%s\",\"totalCapitalAvailable\":%f,\"updatedAt\":\"%s\"}",
-//                email, id, name, picture, totalCapitalAvailable, updatedAt);
-//
-//        mockMvc.perform(put("/customer/updatename").header("authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).content(body)).andDo(print())
-//                .andExpect(status().isOk());
-//
-//    }
-//
-//    /**
-//     * Should update customer email.
-//     *
-//     * @throws Exception the exception
-//     */
-//    @Test
-//    void shouldUpdateCustomerEmail() throws Exception {
-//
-//        String id = "testid";
-//        String name = "new_name";
-//        String email = "newtest@gmail.com";
-//        String picture = "https://s.gravatar.com/avatar/f46f055e3618f5939cfc4a8e6572279c?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fxu.png";
-//        String updatedAt = "2023-10-01T08:19:57.640Z";
-//        int totalCapitalAvailable = 10000;
-//
-//        String body = String.format(
-//                "{\"email\":\"%s\",\"id\":\"%s\",\"name\":\"%s\",\"picture\":\"%s\",\"totalCapitalAvailable\":%d,\"updatedAt\":\"%s\"}",
-//                email, id, name, picture, totalCapitalAvailable, updatedAt);
-//
-//        mockMvc.perform(put("/customer/updateemail").header("authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).content(body)).andDo(print())
-//                .andExpect(status().isOk());
-//
-//    }
-//
-//    /**
-//     * Should update customer capital.
-//     *
-//     * @throws Exception the exception
-//     */
-//    @Test
-//    void shouldUpdateCustomerCapital() throws Exception {
-//        String id = "testid";
-//        String name = "new_name";
-//        String email = "test@gmail.com";
-//        String picture = "https://s.gravatar.com/avatar/f46f055e3618f5939cfc4a8e6572279c?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fxu.png";
-//        String updatedAt = "2023-10-01T08:19:57.640Z";
-//        int totalCapitalAvailable = 12000;
-//
-//        String body = String.format(
-//                "{\"email\":\"%s\",\"id\":\"%s\",\"name\":\"%s\",\"picture\":\"%s\",\"totalCapitalAvailable\":%d,\"updatedAt\":\"%s\"}",
-//                email, id, name, picture, totalCapitalAvailable, updatedAt);
-//
-//        mockMvc.perform(put("/customer/updatecapital").header("authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).content(body)).andDo(print())
-//                .andExpect(status().isOk());
-//
-//    }
-//
-//    /**
-//     * Should delete customer account.
-//     *
-//     * @throws Exception the exception
-//     */
-//    @Test
-//    void shouldDeleteCustomerAccount() throws Exception {
-//
-//        String id = "testid";
-//        mockMvc.perform(delete("/customer/deletecustomer/" + id).header("authorization", "Bearer " + token)).andDo(print()).andExpect(status().isOk());
-//
-//    }
-//
-//    /**
-//     * Should get capital.
-//     *
-//     * @throws Exception the exception
-//     */
-//    @Test
-//    void shouldGetCapital() throws Exception {
-//        String id = "testid";
-//        int expectedCapital = 12000;
-//
-//        mockMvc.perform(get("/customer/getcapital/" + id)
-//                        .header("authorization", "Bearer " + token))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(String.valueOf(expectedCapital)));
-//
-//}
-//
-//}
+        Customer updatedCustomer = new Customer();
+        updatedCustomer.setName(name);
+
+        when(customerService.updateCustomerName(id, name)).thenReturn(null);
+
+        String actualResponse = customerController.updateCustomerName(updatedCustomer);
+
+        assertEquals(null, actualResponse);
+    }
+
+    /**
+     * Should delete customer account.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void shouldDeleteCustomerAccount() throws Exception {
+
+        String userId = "testid";
+        // Mock the behavior of customerService.deleteCustomerAccount
+        when(customerService.deleteCustomerAccount(userId)).thenReturn("AccountDeleted");
+
+        // Call the controller method
+        String actualResponse = customerController.deleteCustomerAccount(userId);
+
+        // Assert
+        assertEquals("AccountDeleted", actualResponse);
+
+    }
+
+    /**
+     * Should get capital.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void shouldGetCapital() throws Exception {
+
+        String userId = "testid";
+        int expectedCapital = 12000;
+
+        when(customerService.getCustomerCapital(userId)).thenReturn(expectedCapital);
+
+        int actualCapital = customerController.getCapital(userId);
+
+        // Assert
+        assertEquals(expectedCapital, actualCapital);
+
+}
+
+}
