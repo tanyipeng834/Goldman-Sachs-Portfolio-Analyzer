29cb10922b63d356c17be0a55af6eec239c685f7
Fadhli <marvelouslyfadhli@gmail.com>
2023-10-22 14:03:19 +0800

    add new portfolio controller test

---
+++ src/test/java/com/trading/application/portfolio/controller/PortfolioControllerTest.java
@@ -0,0 +1,164 @@
+package com.trading.application.portfolio.controller;
+
+import com.trading.application.portfolio.entity.Portfolio;
+import com.trading.application.portfolio.service.PortfolioService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class PortfolioControllerTest {
+
+    @Autowired
+    private PortfolioController portfolioController;
+
+    @MockBean
+    private PortfolioService portfolioService;
+
+    @MockBean
+    private HttpServletRequest request;
+
+    @Test
+    public void testCreatePortfolio() {
+        // Arrange
+        Portfolio portfolio = new Portfolio();
+        when(portfolioService.createPortfolio(portfolio, request)).thenReturn(ResponseEntity.ok("Portfolio created " +
+                "successfully"));
+
+        // Act
+        ResponseEntity<String> response = portfolioController.createPortfolio(portfolio, request);
+
+        // Assert
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Portfolio created successfully", response.getBody());
+    }
+
+
+    @Test
+    void testGetPortfolio() throws ExecutionException, InterruptedException {
+        // Arrange
+        String portfolioId = "123";
+        Portfolio expectedPortfolio = new Portfolio(); // Create a mock portfolio object
+
+        when(portfolioService.getPortfolio(portfolioId)).thenReturn(expectedPortfolio);
+
+        // Act
+        Portfolio result = portfolioController.getPortfolio(portfolioId);
+
+        // Assert
+        assertEquals(expectedPortfolio, result);
+    }
+
+    @Test
+    public void testDeletePortfolio() throws ExecutionException, InterruptedException {
+        // Arrange
+        String portfolioId = "123";
+        ResponseEntity<String> expectedResponse = ResponseEntity.ok("Portfolio deleted successfully");
+
+        when(portfolioService.deletePortfolio(portfolioId)).thenReturn(expectedResponse);
+
+        // Act
+        ResponseEntity<String> response = portfolioController.deletePortfolio(portfolioId);
+
+        // Assert
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testUpdatePortfolio() throws ExecutionException, InterruptedException {
+        // Arrange
+        Portfolio portfolio = new Portfolio();
+        ResponseEntity<String> expectedResponse = ResponseEntity.ok("Portfolio updated successfully");
+
+        when(portfolioService.updatePortfolio(portfolio, request)).thenReturn(expectedResponse);
+
+        // Act
+        ResponseEntity<String> response = portfolioController.updatePortfolio(portfolio, request);
+
+        // Assert
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testGetAllPortfolios() throws ExecutionException, InterruptedException {
+        // Arrange
+        String userId = "123";
+        List<Portfolio> expectedPortfolios = new ArrayList<>(); // Create a list of mock portfolios
+
+        when(portfolioService.getAllPortfolios(userId)).thenReturn(expectedPortfolios);
+
+        // Act
+        List<Portfolio> result = portfolioController.getAllPortfolios(userId);
+
+        // Assert
+        assertEquals(expectedPortfolios, result);
+    }
+
+    @Test
+    public void testGetSectorsByPortfolioId() throws ExecutionException, InterruptedException {
+        // Arrange
+        String portfolioId = "123";
+        Map<String, Integer> expectedSectorCounts = new HashMap<>(); // Create a map of mock sector counts
+        ResponseEntity<Map<String, Integer>> expectedResponse = ResponseEntity.ok(expectedSectorCounts);
+
+        when(portfolioService.getSectorsByPortfolioId(portfolioId)).thenReturn(expectedSectorCounts);
+
+        // Act
+        ResponseEntity<Map<String, Integer>> response = portfolioController.getSectorsByPortfolioId(portfolioId);
+
+        // Assert
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testGetSectorsByUserId() throws ExecutionException, InterruptedException {
+        // Arrange
+        String userId = "123";
+        Map<String, Integer> allSectorCounts = new HashMap<>(); // Create a map of mock sector counts
+        ResponseEntity<Map<String, Integer>> expectedResponse = ResponseEntity.ok(allSectorCounts);
+
+        when(portfolioService.getSectorsByUserId(userId)).thenReturn(allSectorCounts);
+
+        // Act
+        ResponseEntity<Map<String, Integer>> response = portfolioController.getSectorsByUserId(userId);
+
+        // Assert
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testGetAllPublicPortfolios() throws ExecutionException, InterruptedException {
+        // Arrange
+        ArrayList<Portfolio> publicPortfolios = new ArrayList<>(); // Create a list of mock public portfolios
+        ResponseEntity<ArrayList<Portfolio>> expectedResponse = ResponseEntity.ok(publicPortfolios);
+
+        when(portfolioService.getAllPublicPortfolios()).thenReturn(publicPortfolios);
+
+        // Act
+        ResponseEntity<ArrayList<Portfolio>> response = portfolioController.getAllPublicPortfolios();
+
+        // Assert
+        assertEquals(expectedResponse, response);
+    }
+}
